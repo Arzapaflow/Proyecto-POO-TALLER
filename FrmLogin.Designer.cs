2329c673f6e63db8acb5ff2c4078900c9b122161
RICARDO ALBERTO CAAMAL CANUL <a20201457@alumnos.uady.mx>
2026-06-27 14:57:03 -0600

    version donde remuevo las librerias por problemas de compatibilidad

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -73,7 +73,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(429, 128);
+            this.label2.Location = new System.Drawing.Point(394, 128);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(192, 32);
             this.label2.TabIndex = 1;
